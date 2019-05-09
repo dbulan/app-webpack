c0bde4ab8d7f9f0e5281f23c52aa7be89947cb63
dbulan <dimitar.bulanov@gmail.com>
2019-05-09 19:39:43 +0300

    ensure.require

--- src/js/game.js
+++ src/js/game.js
@@ -10,6 +10,31 @@ $(document).ready(function() {
 
   helpers.init(); // jquery test inside helper
   console.log(helpers.isNoU('admin'));
+
+  // plugin kompiliruetsa, no podkluchaetsa tolko pri clicke na knopku
+  document.getElementById('app-require').onclick = function () {
+    // est dva sposoba require.ensure i amd -> require.ensure lutcse
+    
+    // v dannom primere on podgruzitsa srazu bez nazatia na knoplu
+    // let btnclick = require('./modules/btnclick');
+    // btnclick();
+    //
+    // require.ensure
+    require.ensure([], function(require) { // .ensure['module1', 'module2'], ps mozno prosto ostavit' .ensure[] webpack po require pojmet che emu nuzno -> tak i sdelaem
+      let btnclick = require('./modules/btnclick')['default'];
+      btnclick();
+    });
+    //
+    // amd
+    //require(['./modules/btnclick'], function(btnclick) {
+    //  btnclick();
+    //});
+    //
+    // posle npm run dev pojavitsa 0.js (eto kakraz dlya btnclick)
+    // esli ne zagruzaetsa to vozmozno nuzno poigratsa s output publicPath t.k. moduli tipa iz interneta podnjagivajutsa
+    // osobenno esli v /public u menja ierarxia
+    // kstati 0.js t.e. btnclick.js podgruzaetsa tolko pri pervom clicke
+  };
 });
 
 hello('Hi! Hi! Hi!');
---
+++ src/js/modules/btnclick.js
@@ -0,0 +1,6 @@
+function btnclick() {
+  // modul podgruzaetsa i otrabativaet pri tolko pri clicke
+  alert('hello from ensure.require module!')
+};
+   
+export default btnclick;
